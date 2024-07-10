64b3d5b91fa8d84371c19644293ec7f755cfdc63
Lalit Mohan <111294030+Lalit-mp@users.noreply.github.com>
2024-07-10 12:39:45 +0530

    First draft

---
+++ Script.js
@@ -0,0 +1,52 @@
+// const inputs= document.querySelectorAll("input");
+const input = document.querySelector("input");
+const range = document.querySelector(".range");
+const theMassage = document.querySelector(".massage");
+// console.log(theMassage);
+const theOutputText = document.querySelector(".output-text");
+function taxCalculator(ammount) {
+  if (ammount <= 300000) {
+    // console.log("you dont need to pay any tax");
+    theMassage.textContent = "Congratulations- You Don't need to pay any text";
+    theOutputText.textContent = "";
+  } else if (ammount <= 600000) {
+    // console.log("you have to pay 5% tax");
+    theMassage.textContent = " You need to pay 5% Tax on your income";
+    const taxslab5 = (5 / 100) * range.value;
+    theOutputText.textContent = `Your income is ${
+      range.value
+    } So you have to pay ${Math.round(taxslab5)} As income tax`;
+  } else if (ammount <= 900000) {
+    // console.log("15000 + 10% of your income");
+    theMassage.textContent = " You need to pay ₹15000 + 10% Tax on your income";
+    const taxslab5 = (10 / 100) * range.value + 15000;
+    theOutputText.textContent = `Your income is ${range.value} So you have to pay ${Math.round(taxslab5)} As income tax`;
+  } else if (ammount <= 1200000) {
+    // console.log("45000 + 15% of your income");
+    theMassage.textContent = " You need to pay ₹45000 + 15% Tax on your income";
+    const taxslab5 = (15 / 100) * range.value + 45000;
+    theOutputText.textContent = `Your income is ${range.value} So you have to pay ${Math.round(taxslab5)} As income tax`;
+  } else if (ammount <= 1500000) {
+    // console.log("90000 + 20% of your income");
+    theMassage.textContent = " You need to pay ₹90000 + 20% Tax on your income";
+    const taxslab5 = (20 / 100) * range.value + 90000;
+    theOutputText.textContent = `Your income is ${range.value} So you have to pay ${Math.round(taxslab5)} As income tax`;
+  } else if (ammount > 1500000) {
+    // console.log("150000 + 30% of your income");
+    theMassage.textContent =
+      " You need to pay ₹150000 + 30% Tax on your income";
+    const taxslab5 = (30 / 100) * range.value + 150000;
+    theOutputText.textContent = `Your income is ${range.value} So you have to pay ${Math.round(taxslab5)} As income tax`;
+  } else {
+    console.log("wrong entry");
+  }
+}
+
+range.addEventListener("input", () => {
+  input.value = range.value;
+  taxCalculator(input.value);
+});
+input.addEventListener("input", () => {
+  range.value = input.value;
+  taxCalculator(range.value);
+});
