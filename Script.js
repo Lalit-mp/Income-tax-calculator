7e2dc94cdb8694510ec6c05ba50d3b32fa498e88
Lalit Mohan <111294030+Lalit-mp@users.noreply.github.com>
2024-07-12 14:03:33 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,39 +1,36 @@
 
 const input = document.querySelector("input");
 const range = document.querySelector(".range");
-const theMassage = document.querySelector(".massage");
-const theOutputText = document.querySelector(".output-text");
+
+const tList = document.querySelectorAll(".opacity")
 function taxCalculator(ammount) {
-  if (ammount <= 300000) {
-    theMassage.textContent = "Congratulations- You Don't need to pay any text";
-    theOutputText.textContent = "";
-  } else if (ammount <= 600000) {
-    theMassage.textContent = " You need to pay 5% Tax on your income";
-    const taxslab5 = (5 / 100) * range.value;
-    theOutputText.textContent = `Your income is ${
-      range.value
-    } So you have to pay ${Math.round(taxslab5)} As income tax`;
-  } else if (ammount <= 900000) {
-    theMassage.textContent = " You need to pay ₹15000 + 10% Tax on your income";
-    const taxslab5 = (10 / 100) * range.value + 15000;
-    theOutputText.textContent = `Your income is ${range.value} So you have to pay ${Math.round(taxslab5)} As income tax`;
-  } else if (ammount <= 1200000) {
-    theMassage.textContent = " You need to pay ₹45000 + 15% Tax on your income";
-    const taxslab5 = (15 / 100) * range.value + 45000;
-    theOutputText.textContent = `Your income is ${range.value} So you have to pay ${Math.round(taxslab5)} As income tax`;
-  } else if (ammount <= 1500000) {
-    theMassage.textContent = " You need to pay ₹90000 + 20% Tax on your income";
-    const taxslab5 = (20 / 100) * range.value + 90000;
-    theOutputText.textContent = `Your income is ${range.value} So you have to pay ${Math.round(taxslab5)} As income tax`;
-  } else if (ammount > 1500000) {
-    theMassage.textContent =
-      " You need to pay ₹150000 + 30% Tax on your income";
-    const taxslab5 = (30 / 100) * range.value + 150000;
-    theOutputText.textContent = `Your income is ${range.value} So you have to pay ${Math.round(taxslab5)} As income tax`;
-  } 
-  else {
-    console.log("wrong entry");
-  }
+    for (const iterator of tList) {
+        if (ammount == 0) {
+            iterator.style.opacity= 1;
+        }else{
+
+            iterator.style.opacity= 0.5;      
+        }
+    }
+    if ( ammount <= 300000) {
+        tList[0].style.opacity= 1;
+       
+      } else if (ammount <= 600000) {
+        tList[1].style.opacity= 1;
+      } else if (ammount <= 900000) {
+        tList[2].style.opacity= 1;
+      } else if (ammount <= 1200000) {
+        tList[3].style.opacity= 1;
+      } else if (ammount <= 1500000) {
+        tList[4].style.opacity= 1;
+      } else if (ammount > 1500000) {
+        tList[5].style.opacity= 1;
+      } 
+      else {
+        alert("Wrong entry");
+      }
+    
+  
 }
 
 range.addEventListener("input", () => {
@@ -43,9 +40,7 @@ range.addEventListener("input", () => {
 
 input.addEventListener("input", () => {
   if (input.value === "" || input.value === undefined || input.value === null) {
-      range.value = 0;
-      theMassage.textContent = "Please enter your income to Calculate tax";
-      theOutputText.textContent = "";
+      range.value = 0; 
   } else {
       range.value = input.value;
       taxCalculator(input.value);
